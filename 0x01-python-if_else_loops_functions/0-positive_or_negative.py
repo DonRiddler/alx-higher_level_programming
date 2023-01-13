071dd26211aaeb4a1ce7b6d5afa91b6aa10baa80
DonRiddler <umunnabuikeelijah5@gmail.com>
2023-01-13 04:07:24 -0800

    First If else

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,9 +1,27 @@
-#!/usr/bin/python3
-import random
-number = random.randint(-10, 10)
-if number > 0:
-        print(f"{number:d} is positive")
-elif number == 0:
-            print(f"{number:d} is zero")
-else:
-    print(f"{number:d} is negative")
+#include "function_pointers.h"
+
+
+
+/**
+
+ * print_name - Prints a name.
+
+  * @name: The name to be printed.
+
+   * @f: A pointer to a function that prints a name.
+
+    */
+
+    void print_name(char *name, void (*f)(char *))
+
+    {
+
+                if (name == NULL || f == NULL)
+
+                        return;
+
+
+
+                            f(name);
+
+                            }
